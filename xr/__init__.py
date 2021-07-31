391a7b76fcd8053072eb0d774eef6276b5d83050
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-07-31 10:52:21 -0700

    Begin writing rule for overwriting source files. Refine version file.

--- xr/__init__.py
+++ xr/__init__.py
@@ -1,8 +1,11 @@
+from . import version, constants, functions
+
+from .version import *
 from .constants import *
 from .functions import *
 
-from . import constants
-
 __all__ = []
-__all__ += constants.__all__
-__all__ += functions.__all__
+for subpackage in version, constants, functions:
+    __all__ += subpackage.__all__
+
+__version__ = version.PYOPENXR_VERSION  # Not in __all__, right?
