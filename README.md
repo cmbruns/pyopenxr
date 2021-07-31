9d5d0a0942405180dbcfb61a7563834d3ddcddf4
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-07-31 12:01:11 -0700

    Sketch in more potential subpackage structure. Begin learning about pyproject.toml.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pyopenxr
 Unofficial python bindings for OpenXR access to VR and AR devices
+
+This is a work in progress. Stay tuned.
--- xr/library/__init__.py
+++ xr/library/__init__.py
@@ -1,10 +1,8 @@
 import ctypes
 import os
 
-# TODO: loader submodule
 os.add_dll_directory(os.path.dirname(__file__))
-openxr_loader_library = ctypes.cdll.LoadLibrary("openxr_loader.dll")
-print(openxr_loader_library)
+openxr_loader_library = ctypes.cdll.LoadLibrary("openxr_loader")
 
 __all__ = [
     "openxr_loader_library",
