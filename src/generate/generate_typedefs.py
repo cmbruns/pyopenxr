42496cfb772266e86878b1443f8c55c508a4dc80
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-07 12:12:56 -0700

    Begin moving code from generate_typedefs.py into xrg/__init__.py Slightly different openxr_loader.dll ?

--- src/generate/generate_typedefs.py
+++ src/generate/generate_typedefs.py
@@ -4,74 +4,35 @@
 #  * docstrings
 
 import inspect
+import os
 import re
 from typing import Generator
 
 import clang.cindex
 from clang.cindex import CursorKind, Index, Type, TypeKind
 
+from xrg import TypeNameMapper
+
 # These variables are filled in by cmake's configure_file process
 OPENXR_HEADER = "@OPENXR_INCLUDE_FILE@"
 clang.cindex.Config.set_library_file("@LIBCLANG_SHARED_LIBRARY@")
 
+# TODO: remove hard-coded versions
+OPENXR_HEADER = "C:/Program Files/OPENXR/include/openxr/openxr.h"
+if os.path.isfile("C:/Program Files/LLVM/bin/libclang.dll"):
+    clang.cindex.Config.set_library_file("C:/Program Files/LLVM/bin/libclang.dll")
+
 ctypes_imports = {"POINTER", "CFUNCTYPE", "Structure"}
 
 
-def cache_type(t: str) -> str:
-    ctypes_imports.add(t)
-    return t
-
-
-def ctype_from_clang_type(c_type: Type) -> str:
-    m = re.match(r"(?:const )?(u?int(?:8|16|32|64))_t", c_type.spelling)
-    if m:
-        return cache_type(f"c_{m.group(1)}")
-    elif c_type.kind == TypeKind.CHAR_S:
-        return cache_type("c_char")
-    elif c_type.kind == TypeKind.CONSTANTARRAY:
-        return f"({sx(c_type.element_type)} * {c_type.element_count})"
-    elif c_type.kind == TypeKind.ELABORATED:
-        return sx(c_type.get_named_type())
-    elif c_type.kind == TypeKind.ENUM:
-        return cache_type("c_uint32")
-    elif c_type.kind == TypeKind.FLOAT:
-        return cache_type("c_float")
-    elif c_type.kind == TypeKind.POINTER:
-        pt = c_type.get_pointee()
-        if pt.kind == TypeKind.FUNCTIONPROTO:
-            args = [pt.get_result(), *(pt.argument_types())]
-            arg_string = ", ".join([sx(a) for a in args])
-            return f"CFUNCTYPE({arg_string})"
-        elif pt.kind == TypeKind.VOID:
-            return cache_type("c_void_p")
-        else:
-            return f"POINTER({sx(pt)})"
-    elif c_type.kind == TypeKind.RECORD:
-        return c_type.get_declaration().spelling
-    elif c_type.kind == TypeKind.TYPEDEF:
-        return c_type.spelling
-    elif c_type.kind == TypeKind.VOID:
-        return "None"
-    else:
-        assert False
-
-
-# Strip off initial "Xr"
-def ss(s: str) -> str:
-    s = re.sub(r"\bconst\s+", "", s)
-    s = re.sub(r"\bXr", "", s)
-    return s
-
-
-def sx(t: Type) -> str:
-    return ss(ctype_from_clang_type(t))
+type_name_mapper = TypeNameMapper()
 
 
 class CStructField(object):
     def __init__(self, cursor):
         self.cursor = cursor
         self.name = cursor.spelling
-        self.ctypes_type = sx(cursor.type)
+        self.ctypes_type = type_name_mapper.api_type(cursor.type)
 
     def __str__(self):
         return f'\n        ("{self.name}", {self.ctypes_type}),'
@@ -80,7 +41,7 @@ class CStructField(object):
 class CStruct(object):
     def __init__(self, cursor):
         self.cursor = cursor
-        self.name = ss(cursor.spelling)
+        self.name = type_name_mapper.api_type_string(cursor.spelling)
         self.fields = []
         for c in cursor.get_children():
             if c.kind == CursorKind.FIELD_DECL:
@@ -118,11 +79,11 @@ class CStruct(object):
 class CTypedef(object):
     def __init__(self, cursor):
         self.cursor = cursor
-        self.new_type = ss(cursor.spelling)
+        self.new_type = type_name_mapper.api_type_string(cursor.spelling)
         self.name = self.new_type
         clang_type = cursor.underlying_typedef_type
         self.c_type = clang_type.spelling
-        self.ctypes_type = sx(clang_type)
+        self.ctypes_type = type_name_mapper.api_type(clang_type)
 
     def __str__(self) -> str:
         return f"{self.new_type} = {self.ctypes_type}"
@@ -172,11 +133,7 @@ def main():
         # Warning: this file is auto-generated. Do not edit.
         """))
     print("")
-    print(inspect.cleandoc(
-        f"""
-        from ctypes import {", ".join(sorted(ctypes_imports))}
-        """
-    ))
+    print(type_name_mapper.ctypes_import())
     blanks1 = 0
     blanks2 = 0
     for t in typedefs:
--- src/generate/xrg/__init__.py
+++ src/generate/xrg/__init__.py
@@ -1,6 +1,98 @@
-import clang
+"""
+File xrg.__init__.py
+
+This module contains code to help generate the code in pyopenxr.
+"""
+
+import clang.cindex
+from clang.cindex import TypeKind
+import os
+import re
 
 # These variables are filled in by cmake's configure_file process
-OPENXR_HEADER = "@OPENXR_INCLUDE_FILE@"
-clang.cindex.Config.set_library_file("@LIBCLANG_SHARED_LIBRARY@")
+# OPENXR_HEADER = "@OPENXR_INCLUDE_FILE@"
+# if os.path.isfile("@LIBCLANG_SHARED_LIBRARY@"):
+#     clang.cindex.Config.set_library_file("@LIBCLANG_SHARED_LIBRARY@")
 
+# TODO: remove hard-coded versions
+OPENXR_HEADER = "C:/Program Files/OPENXR/include/openxr/openxr.h"
+if os.path.isfile("C:/Program Files/LLVM/bin/libclang.dll"):
+    clang.cindex.Config.set_library_file("C:/Program Files/LLVM/bin/libclang.dll")
+
+
+class TypeNameMapper(object):
+    def __init__(self, use_pyapi=True):
+        # remember names of ctypes types used, for use in import statement
+        self.ctypes_cache = {"POINTER", "CFUNCTYPE", "Structure"}
+        self.use_pyapi = use_pyapi
+
+    def _ctypes_cache(self, ctypes_name: str) -> str:
+        """Remember a ctypes type name in the cache then return it"""
+        self.ctypes_cache.add(ctypes_name)
+        return ctypes_name
+
+    def api_type_string(self, c_type_name: str) -> str:
+        if self.use_pyapi:
+            return self.pyapi_type_name(c_type_name)
+        else:
+            return self.capi_type_name(c_type_name)
+
+    def api_type(self, clang_type: clang.cindex.Type) -> str:
+        """Return API-specific (either pythonic or C-like) type name corresponding to parsed clang type"""
+        return self.api_type_string(self.ctypes_name(clang_type))
+
+    @staticmethod
+    def capi_type_name(c_type_name: str, snake=False) -> str:
+        """The low level C-like api uses the exact same names as in C"""
+        s = re.sub(r"\b(?:const|volatile)\s+", "", c_type_name)  # But without const
+        return s
+
+    def ctypes_import(self) -> str:
+        """Return import statement for used ctypes types"""
+        return f"from ctypes import {', '.join(sorted(self.ctypes_cache))}"
+
+    def ctypes_name(self, clang_type: clang.cindex.Type) -> str:
+        """Return the ctypes type name corresponding to a parsed clang type."""
+        m = re.match(r"(?:const )?(u?int(?:8|16|32|64))_t", clang_type.spelling)
+        if m:
+            return self._ctypes_cache(f"c_{m.group(1)}")
+        elif clang_type.kind == TypeKind.CHAR_S:
+            return self._ctypes_cache("c_char")
+        elif clang_type.kind == TypeKind.CONSTANTARRAY:
+            return f"({self.api_type(clang_type.element_type)} * {clang_type.element_count})"
+        elif clang_type.kind == TypeKind.ELABORATED:
+            return self.api_type(clang_type.get_named_type())
+        elif clang_type.kind == TypeKind.ENUM:
+            return self._ctypes_cache("c_uint32")
+        elif clang_type.kind == TypeKind.FLOAT:
+            return self._ctypes_cache("c_float")
+        elif clang_type.kind == TypeKind.POINTER:
+            pt = clang_type.get_pointee()
+            if pt.kind == TypeKind.FUNCTIONPROTO:
+                args = [pt.get_result(), *(pt.argument_types())]
+                arg_string = ", ".join([self.api_type(a) for a in args])
+                return f"CFUNCTYPE({arg_string})"
+            elif pt.kind == TypeKind.VOID:
+                return self._ctypes_cache("c_void_p")
+            else:
+                return f"POINTER({self.api_type(pt)})"
+        elif clang_type.kind == TypeKind.RECORD:
+            return clang_type.get_declaration().spelling
+        elif clang_type.kind == TypeKind.TYPEDEF:
+            return clang_type.spelling
+        elif clang_type.kind == TypeKind.VOID:
+            return "None"
+        else:
+            assert False
+
+    @staticmethod
+    def pyapi_type_name(c_type_name: str, snake=False) -> str:
+        """Convert raw C symbol name to pythonic name"""
+        s = TypeNameMapper.capi_type_name(c_type_name)
+        s = re.sub(r"\bXr", "", s)
+        return s
+
+
+__all__ = [
+    "TypeNameMapper",
+]
