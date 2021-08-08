d72848320f3c3c9baab720198791bc3ff1db9e56
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-08 09:46:28 -0700

    Update print_openxr_version.py to use xrg.

--- src/generate/print_openxr_version.py
+++ src/generate/print_openxr_version.py
@@ -1,12 +1,9 @@
 # This script prints the current version of the OpenXR API
 
 import re
+import xrg
 
-# These variables are filled in by cmake's configure_file process
-openxr_header = "@OPENXR_INCLUDE_FILE@"
-
-with open(openxr_header) as f:
-    file_string = f.read()
+file_string = xrg.get_header_as_string()
 
 # We expect a line in openxr.h like
 #   "#define XR_CURRENT_API_VERSION XR_MAKE_VERSION(1, 0, 17)"
