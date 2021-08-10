087ec994167f7a941e2ca7afb97785ed91aeebf9
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-10 16:22:36 -0700

    Fuss with exceptions.

--- src/xr/exceptions.py
+++ src/xr/exceptions.py
@@ -1,58 +1,79 @@
-from typing import Union
+from typing import Optional, Union
 from .enums import Result
 
+# raise_on_qualified_success is a module setting to control whether positive non-SUCCESS result
+# trigger exceptions.
 raise_on_qualified_success = True
 
 
 class XrException(Exception):
     """Base class for all OpenXR exceptions."""
-
     @staticmethod
-    def is_exception():
+    def is_exception() -> bool:
         return True
 
 
-class XrErrorResult(XrException):
+class ResultException(XrException):
+    """Exception related to return value of and OpenXR function."""
+    @staticmethod
+    def get_result_enum() -> Optional[Result]:
+        return None
+
+
+class ResultError(ResultException):
     """Error during OpenXR function call."""
 
 
-class XrQualifiedSuccessResult(XrException):
+class QualifiedSuccessResult(ResultException):
     """An OpenXR function returned a non-error status other than SUCCESS"""
-
     @staticmethod
-    def is_exception():
+    def is_exception() -> bool:
         return raise_on_qualified_success
 
 
-class XrSuccessResult(object):
+class SuccessResult(ResultException):
     """An OpenXR function call completed successfully."""
-    def __init__(self, _unused: str = None):
-        pass
+    @staticmethod
+    def is_exception() -> bool:
+        return False
 
     @staticmethod
-    def is_exception():
-        return False
+    def get_result_enum() -> Result:
+        return Result.SUCCESS
+
+
+class TimeoutExpired(QualifiedSuccessResult):
+    @staticmethod
+    def get_result_enum() -> Result:
+        return Result.TIMEOUT_EXPIRED
+
+
+class ValidationFailureError(ResultError):
+    @staticmethod
+    def get_result_enum() -> Result:
+        return Result.ERROR_VALIDATION_FAILURE
 
 
 # TODO: specific exceptions for every result type
 
 
 _exception_map = {
-    Result.SUCCESS: XrSuccessResult,
+    Result.SUCCESS: SuccessResult,
+    Result.TIMEOUT_EXPIRED: TimeoutExpired,
 }
 
 
-def check_result(xr_result_int: int, message: str = None) -> Union[XrException, XrSuccessResult]:
+def check_result(xr_result_int: int, message: str = None) -> Union[XrException, SuccessResult]:
     xr_result_enum = Result(xr_result_int)
     if xr_result_enum in _exception_map:
         xr_result_exception = _exception_map[xr_result_enum]
     else:
         if xr_result_int < 0:
-            xr_result_exception = XrErrorResult
+            xr_result_exception = ResultError
         elif xr_result_int > 1:
-            xr_result_exception = XrQualifiedSuccessResult
+            xr_result_exception = QualifiedSuccessResult
         else:
-            xr_result_exception = XrSuccessResult
+            xr_result_exception = SuccessResult
     if message is None:
         return xr_result_exception()
     else:
@@ -62,7 +83,8 @@ def check_result(xr_result_int: int, message: str = None) -> Union[XrException, 
 __all__ = [
     "check_result",
     "raise_on_qualified_success",
-    "XrErrorResult",
+    "ResultError",
     "XrException",
-    "XrQualifiedSuccessResult",
+    "ResultException",
+    "QualifiedSuccessResult",
 ]
