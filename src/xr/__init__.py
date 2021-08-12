e6bd606dff31b019a47b9866d8b53395777f1f61
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-11 21:43:25 -0700

    Begin sketching in tests.

--- src/xr/__init__.py
+++ src/xr/__init__.py
@@ -3,10 +3,11 @@ from . import version, constants, enums, functions
 from .version import *
 from .constants import *
 from .enums import *
+from .typedefs import *
 from .functions import *
 
 __all__ = []
-for subpackage in version, constants, enums, functions:
+for subpackage in version, constants, enums, typedefs, functions:
     __all__ += subpackage.__all__
 
 __version__ = version.PYOPENXR_VERSION  # Not in __all__, right?
---
+++ tests/test_extension_properties.py
@@ -0,0 +1,19 @@
+import unittest
+
+import xr
+
+
+class TestExtensionProperties(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_construct(self):
+        ep = xr.ExtensionProperties()
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
