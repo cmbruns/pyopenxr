a7a30660bffbc895c65790b00c5501cf491ce416
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-11 21:55:34 -0700

    Fix nosetests problem.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,4 @@
 # Programatically run as if "nosetests" on command line
-# TODO: this is not working. I can run test_extenstion_properties.py
-# and get a test result. But nosetests and this script run zero tests.
 
 import nose
 nose.main()
