1053b0987d4c4a71a2651be3715ea76f67d03d2b
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-14 11:43:30 -0700

    Move hand-coded contents of functions.py into experiment.py, making way for future generated functions.py. Add comment to run_tests.py Fix #8

--- src/xr/__init__.py
+++ src/xr/__init__.py
@@ -6,8 +6,10 @@ from .enums import *
 from .typedefs import *
 from .functions import *
 
+from .experiment import *
+
 __all__ = []
-for subpackage in version, constants, enums, typedefs, functions:
+for subpackage in version, constants, enums, typedefs, functions, experiment:
     __all__ += subpackage.__all__
 
 __version__ = version.PYOPENXR_VERSION  # Not in __all__, right?
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,4 +1,5 @@
 # Programatically run as if "nosetests" on command line
+# NOTE: run this from the top level pyopenxr folder, *NOT* from this "tests" folder.
 
 import nose
 nose.main()
