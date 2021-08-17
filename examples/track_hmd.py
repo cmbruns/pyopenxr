3c54268bc23e6c5dea900cac65ab8c7a6348dd16
Christopher Bruns <cmbruns@rotatingpenguin.com>
2021-08-16 18:34:47 -0700

    Tiny progress toward track_hmd.py

--- examples/track_hmd.py
+++ examples/track_hmd.py
@@ -4,6 +4,5 @@ import xr
 # https://github.com/jherico/OpenXR-Samples/blob/master/src/examples/sdl2_gl_single_file_example_c.cpp
 
 props = xr.enumerate_instance_extension_properties()
-print(len(props))
-for p in props:
-    print(p.extension_name)
+assert "XR_KHR_opengl_enable" in props  # TODO: where is symbol XR_KHR_OPENGL_ENABLE_EXTENSION_NAME
+requested_extensions = ["XR_KHR_opengl_enable"]
