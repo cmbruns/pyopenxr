ff3448eb5e842e43a23fb502c12d09fd6dcc476e
Christopher Bruns <cmbruns@rotatingpenguin.com>
2022-06-20 15:09:02 -0700

    Change symbolic layer names to better match pattern used for symbolic extension names.

--- src/xr/api_layer/__init__.py
+++ src/xr/api_layer/__init__.py
@@ -7,8 +7,8 @@ from .dynamic_api_layer_base import *
 from . import loader_interfaces, dynamic_api_layer_base  # For __all__ handling
 
 # Expose symbolic names for packaged api layers
-XR_APILAYER_LUNARG_api_dump_NAME = "XR_APILAYER_LUNARG_api_dump"
-XR_APILAYER_LUNARG_core_validation_NAME = "XR_APILAYER_LUNARG_core_validation"
+LUNARG_api_dump_APILAYER_NAME = "XR_APILAYER_LUNARG_api_dump"
+LUNARG_core_validation_APILAYER_NAME = "XR_APILAYER_LUNARG_core_validation"
 
 # Automatically expose packaged API layers for supported platforms
 if platform.system() in ["Windows", "Linux"]:
@@ -17,8 +17,8 @@ if platform.system() in ["Windows", "Linux"]:
 
 __all__ = [
     "expose_packaged_api_layers",
-    "XR_APILAYER_LUNARG_api_dump_NAME",
-    "XR_APILAYER_LUNARG_core_validation_NAME",
+    "LUNARG_api_dump_APILAYER_NAME",
+    "LUNARG_core_validation_APILAYER_NAME",
 ]
 __all__ += loader_interfaces.__all__
 __all__ += dynamic_api_layer_base.__all__
