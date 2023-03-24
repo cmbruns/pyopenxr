692ea9fa9e68e34fdf453d897cc659d4682e5e02
Christopher Bruns <cmbruns@rotatingpenguin.com>
2023-03-23 19:30:13 -0700

    Update for OpenXR 1.0.27 on Windows. Use pytest instead of nosetests, for compatibility with python 3.11

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,9 +1,6 @@
-# Programmatically run as if "nosetests" on command line
+# Programmatically run as if "pytest" on command line
 # NOTE: run this from the top level pyopenxr folder, *NOT* from this "tests" folder.
 
-# workaround for python 3.10 and nose
-import collections.abc
-collections.Callable = collections.abc.Callable
-import nose
+import pytest
 
-nose.main()
+retcode = pytest.main()
