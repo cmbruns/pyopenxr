b11eb1da19e1c41011022acc67d735aa017e4686
Christopher Bruns <cmbruns@rotatingpenguin.com>
2023-07-23 14:12:24 -0700

    Delete api3 manager objects, in favor of new context managers for handle types.

--- src/xr/api3/__init__.py
+++ src/xr/api3/__init__.py
@@ -1,7 +1,5 @@
 from .event_bus import *
-from .instance_manager import *
 from .opengl_graphics import *
-from .session_manager import *
 from .time_fetcher import *
 from .two_controllers import *
 
