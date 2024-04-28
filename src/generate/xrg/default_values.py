cb9ea46821fb44db1150ae8934d79c4c8fcb4a35
Christopher Bruns <cmbruns@rotatingpenguin.com>
2024-04-28 14:19:08 -0700

    Update code generation to use 1.0.x api_version default that was added manually. Remove manually added comment.

--- src/generate/xrg/default_values.py
+++ src/generate/xrg/default_values.py
@@ -7,7 +7,7 @@ default_values = {
                 "application_name": "os.path.basename(sys.argv[0])",
                 "engine_name": '"pyopenxr"',
                 "engine_version": "PYOPENXR_CURRENT_API_VERSION",
-                "api_version": "XR_CURRENT_API_VERSION",
+                "api_version": "Version(1, 0, XR_VERSION_PATCH)",
             }
         },
         "InstanceCreateInfo": {
