334341b6a08fd0dd27b228c49ec34ed5c67170e5
Christopher Bruns <cmbruns@rotatingpenguin.com>
2025-11-30 15:08:26 -0800

    Experimental change to egl initialization to see if it helps CI.

--- tests/test_egl.py
+++ tests/test_egl.py
@@ -5,12 +5,12 @@ class TestEgl(unittest.TestCase):
     def test_basic_synopsis(self):
         try:
             from OpenGL import EGL
-            from xr.utils.gl import egl_util
-            context_provider = egl_util.EGLOffscreenContextProvider()
-            with context_provider.scope():
-                _binding = egl_util.EGLGraphicsBinding(context_provider)
         except AttributeError:
-            pass  # Windows does not have EGL
+            return  # Windows does not have EGL
+        from xr.utils.gl import egl_util
+        context_provider = egl_util.EGLOffscreenContextProvider()
+        with context_provider.scope():
+            _binding = egl_util.EGLGraphicsBinding(context_provider)
 
 
 if __name__ == '__main__':
